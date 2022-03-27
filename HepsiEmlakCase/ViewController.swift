ec1b43a8bfbb748e6b0c9602d40db870914070da
burhansezeroglu <burhan.sezeroglu@misli.com>
2022-03-27 13:45:33 +0300

    Initial Commit

---
+++ HepsiEmlakCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HepsiEmlakCase
+//
+//  Created by cem sezeroglu on 27.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
