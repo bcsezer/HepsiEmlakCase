ac02bef8617679fd3299b17332320854d105dbdb
burhansezeroglu <burhan.sezeroglu@misli.com>
2022-04-04 00:16:22 +0300

    EmptyCell push

---
+++ HepsiEmlakCase/Screens/Basket/Cells/EmptyBasketCell.swift
@@ -0,0 +1,12 @@
+//
+//  EmptyBasketCell.swift
+//  HepsiEmlakCase
+//
+//  Created by cem sezeroglu on 3.04.2022.
+//
+
+import UIKit
+
+class EmptyBasketCell: UITableViewCell {
+   static let identifier = "EmptyBasketCell"
+}
