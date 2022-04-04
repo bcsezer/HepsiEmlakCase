1a14619ac327a9d77a9b60b82eae267c20942eec
burhansezeroglu <burhan.sezeroglu@misli.com>
2022-04-04 21:31:50 +0300

    place order view edited

--- HepsiEmlakCase/Screens/Basket/BasketViewController.swift
+++ HepsiEmlakCase/Screens/Basket/BasketViewController.swift
@@ -22,12 +22,16 @@ class BasketViewController: UIViewController, BasketViewDisplayLogic {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var myBasketTitle: UILabel!
     @IBOutlet weak var totalPrice: UILabel!
+    @IBOutlet weak var totalPriceView: UIView!
+    @IBOutlet weak var placeOrderButton: UIButton!
+    @IBOutlet weak var placeOrderView: UIView!
     
     var basketItems: [BasketViewModels.BasketViewCell] = []
     
     private struct Constant {
         static let heightForRow: CGFloat = 147.0
         static let emptyCellHeight: CGFloat = 200.0
+        static let cornerRaious: CGFloat = 4.0
     }
     
     override func viewDidLoad() {
@@ -36,11 +40,38 @@ class BasketViewController: UIViewController, BasketViewDisplayLogic {
         EmptyBasketCell.registerWithNib(to: tableView)
         tableView.delegate = self
         tableView.dataSource = self
+        apperance()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         interactor?.handle(request: BasketViewModels.GetBasketList.Request())
+    }
+    
+    private func apperance() {
+        totalPriceView.style(
+            style: ViewStyle(
+                backgroundColor: Colors.white,
+                tintColor: Colors.red,
+                layerStyle: ViewStyle.LayerStyle(
+                    masksToBounds: true,
+                    cornerRadius: Constant.cornerRaious,
+                    borderStyle: nil
+                )
+            )
+        )
+        
+        placeOrderButton.style(
+            style: ViewStyle(
+                backgroundColor: Colors.white,
+                tintColor: Colors.red,
+                layerStyle: ViewStyle.LayerStyle(
+                    masksToBounds: true,
+                    cornerRadius: Constant.cornerRaious,
+                    borderStyle: nil
+                )
+            )
+        )
     }
     
     @IBAction func tapPlaceOrder(_ sender: UIButton) {
@@ -52,6 +83,7 @@ class BasketViewController: UIViewController, BasketViewDisplayLogic {
         self.basketItems = viewModel.cell
         myBasketTitle.text = "Sepetim (\(basketItems.count))"
         totalPrice.text = viewModel.totalPrice
+        placeOrderView.isHidden = false
         self.tableView.reloadData()
     }
     
@@ -78,6 +110,7 @@ class BasketViewController: UIViewController, BasketViewDisplayLogic {
     
     func display(viewModel: BasketViewModels.EmptyList.ViewModel) {
         self.basketItems = viewModel.cell
+        placeOrderView.isHidden = true
         tableView.isScrollEnabled = false
         tableView.reloadData()
     }
